fcd72ee2ccb27907760c5585d35fa5a46caf6e72
TienHuuHutech <daohuutien2004@gmail.com>
2026-03-11 08:13:58 +0700

    Tinh tong 2 so (doi float sang int)

---
+++ main.py
@@ -0,0 +1,12 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+def main():
+    a = int(input("Nhập số a: "))
+    b = int(input("Nhập số b: "))
+    
+    result = sum_two_numbers(a, b)
+    print("Tổng của a và b là:", result)
+
+if __name__ == "__main__":
+    main()
